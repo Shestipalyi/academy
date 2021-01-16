a47bf4cea9bfbfa087f93c4cc80ba713b03cb2b1
Shestipalyi <77542322+Shestipalyi@users.noreply.github.com>
2021-01-16 20:13:07 +0300

    Update Main.java

--- academy/src/by/academy/lesson2/Main.java
+++ academy/src/by/academy/lesson2/Main.java
@@ -3,7 +3,7 @@ package by.academy.lesson2;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Second commit!");
+		System.out.println("Second commit");
 		// TODO Auto-generated method stub
 
 	}
