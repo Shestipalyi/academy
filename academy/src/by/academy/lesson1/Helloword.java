99ab6403b70745fcf4387cacaff6c4711bbedc37
Исаков <a_isakov_mmp@mail.ru>
2021-01-16 19:43:29 +0300

    First comment

---
+++ academy/src/by/academy/lesson1/Helloword.java
@@ -0,0 +1,9 @@
+package by.academy.lesson1;
+
+public class Helloword {
+
+	public static void main(String[] args) {
+		System.out.println("Hello academy students!");
+	}
+
+}
---
+++ academy/src/by/academy/lesson1/Test.java
@@ -0,0 +1,8 @@
+package by.academy.lesson1;
+
+public class Test {
+	public static void main(String[] args) {
+		int age = 1;
+		System.out.println(age);
+	}
+}
