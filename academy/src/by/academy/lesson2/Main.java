f995cadb9fffb8384635a5c7e3704c85105f60df
Andrei <a_isakov_mmp@mail.ru>
2021-01-16 19:51:04 +0300

    Second comment

---
+++ academy/src/by/academy/lesson2/Main.java
@@ -0,0 +1,11 @@
+package by.academy.lesson2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Second commit!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
